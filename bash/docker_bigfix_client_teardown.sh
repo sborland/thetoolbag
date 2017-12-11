f60b2d9d5931f76c2e661b6b36653bd62d07633b
Sarah Borland <borlandsab@gmail.com>
2017-12-11 09:29:54 -0800

    bash teardown clean up

--- bash/docker_bigfix_client_teardown.sh
+++ bash/docker_bigfix_client_teardown.sh
@@ -7,7 +7,6 @@
 #OPERATING_SYSTEM=
 
 id=$(docker ps --filter "name=${ROOT_SERVER_ADDRESS}_${OPERATING_SYSTEM}" --format "{{.ID}}" 2>&1)
-if [ -z != $id ]; then
+if [ -n $id ]; then
       echo "Stopping container id: "; docker stop $id; docker rm -f $id
-      #id=$(docker ps --filter "name=${ROOT_SERVER_ADDRESS}_${OPERATING_SYSTEM}" --format "{{.ID}}" 2>&1)
 fi
