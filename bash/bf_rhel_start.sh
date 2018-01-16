f54087c21a55fd5a39a44632de02b9f85ab22991
Sarah Borland <borlandsab@gmail.com>
2018-01-15 16:26:09 -0800

    small start & stop scripts for bf

---
+++ bash/bf_rhel_start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+service besserver start
+service besfilldb start
+service besgatherdb start
+service beswebreports start
+service besclient start
+service beswebui start
---
+++ bash/bf_rhel_stop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+service beswebui stop
+service besclient stop
+service beswebreports stop
+service besgatherdb stop
+service besfilldb stop
+service besserver stop
